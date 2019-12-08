c1e3634b900e09cfe5359b3d98fbe36ecd5c385b
Loi <loia5tqd001@gmail.com>
2019-12-08 17:02:24 +0700

    Checkout Netflix Carousel

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -18,42 +18,7 @@
     </v-row>
 
     <v-row>
-        <v-slide-group
-          v-model="model"
-          class="pa-4"
-        >
-          <v-slide-item
-            v-for="n in 5"
-            :key="n"
-            #default="{ active, toggle }"
-          >
-            <v-col cols="6">
-              <card-browse
-                :color="active ? 'primary' : 'grey lighten-1'"
-                class="ma-4"
-                height="200"
-                @click="toggle"
-                srcImg="../4.jpg"
-                title=""
-              >
-                <v-row
-                  class="fill-height"
-                  align="center"
-                  justify="center"
-                >
-                  <v-scale-transition>
-                    <v-icon
-                      v-if="active"
-                      color="white"
-                      size="48"
-                      v-text="'mdi-close-circle-outline'"
-                    ></v-icon>
-                  </v-scale-transition>
-                </v-row>
-              </card-browse>
-            </v-col>
-          </v-slide-item>
-        </v-slide-group>
+      <netflix-carousel></netflix-carousel>
     </v-row>
   </v-container>
 </template>
@@ -61,11 +26,13 @@
 <script>
 import CardHome from "~/components/CardHome";
 import CardBrowse from "~/components/CardBrowse";
+import NetflixCarousel from '~/components/NetflixCarousel'
 
 export default {
   components: {
     CardHome,
-    CardBrowse
+    CardBrowse,
+    NetflixCarousel
   },
   data() {
     return {};
