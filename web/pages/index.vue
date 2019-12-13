d69f5a30a561ac450a648ced9d8ab5b0b8ee3b09
Loi <loia5tqd001@gmail.com>
2019-12-13 11:02:21 +0700

    Add images

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,39 +1,43 @@
 <template>
   <v-container>
+
     <v-row>
       <v-col cols="12" sm="8" md="6">
         <card-home
-          srcImg="../3.jpg"
+          srcImg="../thumbnail-horizontal/3.jpg"
           title="This is the title of the movie"
         ></card-home>
       </v-col>
 
       <v-col cols="12" sm="8" md="6">
         <card-home
-          srcImg="../3.jpg"
+          srcImg="../thumbnail-horizontal/3.jpg"
           title="This is the title of the movie"
         ></card-home>
       </v-col>
-
     </v-row>
 
     <v-row>
       <carousel></carousel>
+      <carousel></carousel>
+      <carousel></carousel>
+      <carousel></carousel>
+      <carousel></carousel>
     </v-row>
+
   </v-container>
 </template>
 
 <script>
 import CardHome from "~/components/CardHome";
 import CardBrowse from "~/components/CardBrowse";
-// import NetflixCarousel from '~/components/NetflixCarousel'
-import Carousel from '~/components/Carousel'
+import Carousel from "~/components/Carousel";
 
 export default {
   components: {
     CardHome,
     CardBrowse,
-    Carousel
+    Carousel,
   },
   data() {
     return {};
