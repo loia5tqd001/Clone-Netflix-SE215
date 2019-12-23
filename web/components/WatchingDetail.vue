ce96a867fc652c351bcda67391354f51f2c53470
Loi <loia5tqd001@gmail.com>
2019-12-23 09:32:07 +0700

    update watching detail

--- web/components/WatchingDetail.vue
+++ web/components/WatchingDetail.vue
@@ -10,6 +10,59 @@
 
     <div class="WatchingDetail-details">
 
+      <h1 class="WatchingDetail-title">
+        the nucraker and <br> the four realms
+      </h1>
+
+      <p class="WatchingDetail-categories">
+        Adventure, Family, Fantasy
+      </p>
+
+      <v-rating readonly value="4" color="red"></v-rating>
+
+      <p class="WatchingDetail-info">
+        <span><span>Year</span><br>2018</span>
+        <span><span>Country</span><br>USA</span>
+        <span><span>Length</span><br>112min</span>
+      </p>
+
+      <div class="WatchingDetail-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, atque nostrum! Quod perferendis natus voluptatum nam alias, eos, vero molestias consequuntur officia eum nesciunt neque at labore eius officiis! Vero maiores temporibus animi consequatur, quibusdam quidem recusandae et necessitatibus fuga laboriosam in, perferendis cumque veniam? Odio corrupti
+      </div>
+
+      <div class="WatchingDetail-action-bar">
+        <nuxt-link to="/recently-added" class="button --add">
+          <v-icon title="add to list">mdi-plus</v-icon>
+        </nuxt-link>
+
+        <nuxt-link to="/share" class="button --share">
+          <v-icon title="share">mdi-share-variant</v-icon>
+        </nuxt-link>
+
+        <nuxt-link to="/reviews" class="button --reviews">
+          <span title="watch reviews">Reviews</span>
+        </nuxt-link>
+      </div>
+
+      <div class="WatchingDetail-screenshots">
+        <div class="__title">
+          <span>Screenshots</span>
+          <v-icon class="__icon">mdi-arrow-right</v-icon>
+        </div>
+        <div class="__carousel">
+          <div class="__images">
+            <div class="__img">
+              <img src="@/static/screenshots/1.jpg">
+            </div>
+            <div class="__img">
+              <img src="@/static/screenshots/2.jpg">
+            </div>
+            <div class="__img">
+              <img src="@/static/screenshots/3.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
 
@@ -34,6 +87,20 @@
     margin: 0 auto 1rem;
   }
 
+  @mixin hover-overflow {
+    overflow: hidden;
+
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+      transition: transform .5s;
+    }
+    &:hover img {
+      transform: scale(1.1);
+    }
+  }
+
   .WatchingDetail-thumbnail {
     height: 100%;
     position: relative;
@@ -43,18 +110,7 @@
       width: 100%;
       height: 100%;
       clip-path: ellipse(70% 85% at 30% 50%);
-      overflow: hidden;
-
-      img {
-        display: block;
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-        transition: transform .5s;
-      }
-      &:hover img {
-        transform: scale(1.1);
-      }
+      @include hover-overflow;
 
       &::after {
         content: '';
@@ -68,20 +124,113 @@
       position: absolute;
       top: 50%;
       right: 0;
-      transform: scale(2);
+      transform: translateY(-50%) scale(2);
       cursor: pointer;
       transition: transform .2s;
 
       &:hover {
-        transform: scale(2.2);
+        transform: translateY(-50%) scale(2.2);
       }
     }
   }
 
   .WatchingDetail-details {
-    /* background: blue; */
     height: 100%;
+    text-align: center;
+    padding: 1rem 1rem;
 
+    .WatchingDetail-title {
+      text-transform: uppercase;
+    }
+
+    .WatchingDetail-categories {
+      opacity: .8;
+      margin: 0 0 .5rem;
+    }
+
+    .WatchingDetail-info {
+      display: flex;
+      justify-content: space-between;
+      padding: 0 15rem;
+      margin: 1rem 0 0 1.5rem;
+      font-weight: 600;
+
+      span span:first-child {
+        opacity: .6;
+        margin-right: .3rem;
+      }
+    }
+
+    .WatchingDetail-description {
+      padding: 0 7rem;
+      margin: 1rem 0;
+      text-align: justify;
+    }
+
+    .WatchingDetail-action-bar {
+      display: flex;
+      margin: 0 7rem;
+
+      .button {
+        text-decoration: none;
+        color: rgba(white,.8);
+        font-weight: bold;
+        display: inline-block;
+        margin: 0 .3rem;
+        transition: transform .2s;
+
+        
+        &:hover {
+          transform: translateY(-2px) scale(1.05);
+        }
+
+        &.--reviews {
+          margin-left: auto;
+        }
+      }
+    }
+    
+    .WatchingDetail-screenshots {
+
+      .__title {
+        margin: 1rem 7rem 0;
+        display: flex;
+        justify-content: space-between;
+
+        .__icon {
+          transition: transform .2s;
+          cursor: pointer;
+
+          &:hover {
+            transform: translateY(-2px) scale(1.05);
+          }
+        }
+      }
+
+      .__carousel {
+        width: 100%;
+      }
+
+      .__images {
+        max-width: 100%;
+        height: 160px;
+        display: grid;  
+        grid-auto-flow: column;
+        margin: 1rem 3rem 0;
+        justify-content: center;
+        grid-auto-columns: minmax(220px, 1fr);
+        grid-gap: 10px;
+        overflow-x: auto;
+
+        .__img {
+          @include hover-overflow;
+          height: 100%;
+
+          border-radius: .5rem;
+          box-shadow: $standard-shadow;
+        }
+      }
+    }
   }
 
 </style>
