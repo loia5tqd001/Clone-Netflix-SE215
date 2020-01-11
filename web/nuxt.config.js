f54c0f5dc0007a8ba2424fb20b03a19bddda1e69
Loi <loia5tqd001@gmail.com>
2020-01-11 12:58:19 +0700

    fix: fix snackbar for build mode in nuxt
    followed this: https://github.com/eolant/vuetify-toast-snackbar/issues/33#issuecomment-573279668

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -70,7 +70,7 @@ export default {
    */
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
-    treeShaking: false,
+    treeShake: false,
     theme: {
       dark: true,
       themes: {
