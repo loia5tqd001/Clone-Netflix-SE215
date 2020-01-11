6072b06e8f79ed4cc9df723dcef16a7135589578
Loi <loia5tqd001@gmail.com>
2020-01-11 09:45:17 +0700

    imp: change search by keywords functionality

--- web/pages/search.vue
+++ web/pages/search.vue
@@ -1,6 +1,16 @@
 <template>
   <div light class="search-container">
-    <p>All movies, categories, tv shows related to: <strong>{{ searchText }}</strong></p>
+    <p>Explorer titles related to: 
+      <a class="search-key" 
+         v-for="(searchKey, i) in searchKeys.slice(0, ~~(searchText ? searchText.length : 0))" 
+         :key="searchKey"
+         @click.prevent="changeSearchKey(i)"
+      >
+        <strong :class="{ 'current-search-key': i === curSelectedSearchKey }" > 
+          {{ searchKey }} 
+        </strong>
+      </a>
+    </p>
     <transition-group class="search-movie-grid" tag="div" name="fade" mode="out-in">
       <MovieCard v-for="movie in movies" :key="movie.id" :movie=movie ></MovieCard>
     </transition-group>
@@ -19,7 +29,21 @@ export default {
   data() {
     return {
       movies: this.getMovies(),
-      searchText: this.$store.getters.getSearchText
+      searchText: this.$store.getters.getSearchText,
+      curSelectedSearchKey: 0,
+      searchKeys: [
+        'Horror Movies',
+        'Horror',
+        'Thriller & Horror',
+        'Chilling Horror Movies',
+        'Psychological Horror Movies',
+        'Demon Horror Movies',
+        'Horror Comedies',
+        'Supernatural Hornor Movies',
+        'The Last Horror Film',
+        'Horror Comedies',
+        'Movies About Horses'
+      ]
     }
   },
   methods: {
@@ -29,10 +53,27 @@ export default {
         ...movie
       }))
     },
-    reload: _.debounce(function() {
+    changeSearchKey(i) {
+      if (i === this.curSelectedSearchKey) return
+  
+      this.reloadWith(i)
+    },
+
+    reloadWith: _.debounce(function(index) {
+      this.curSelectedSearchKey = index
+      
       this.movies = _.shuffle(this.getMovies()),
       this.searchText = this.$store.getters.getSearchText
-    }, 500)
+    }, 
+    100),
+
+    reload: _.debounce(function() {
+      this.curSelectedSearchKey = 0
+
+      this.movies = _.shuffle(this.getMovies()),
+      this.searchText = this.$store.getters.getSearchText
+    }, 
+    500)
   },
   watch: {
     // stackoverflow.com/questions/43270159/vuejs-2-how-to-watch-store-values-from-vuex
@@ -55,6 +96,30 @@ export default {
     grid-column-gap: 1rem;
     padding: 1rem 0;
   }
+
+  .search-key {
+    color: white;
+
+    transition: .2s;
+    
+    &:hover {
+      text-shadow: 2px 2px 4px #000, -2px -2px 4px #fff4;
+    }
+  }
+  .search-key:not(:last-child)::after {
+    content: ' | ';
+    background: transparent !important;
+  }
+  .current-search-key {
+    background: red;
+
+    transition: .2s;
+    
+    &:hover {
+      text-shadow: 2px 2px 4px #0007, -2px -2px 4px #fff7;
+    }
+  }
+
 
   .fade-enter-active, .fade-leave-active {
     transition: opacity .2s;
